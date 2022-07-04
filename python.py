afad5525563e2720d9fc1ba452db677c7bd9f720
Jaime Dargallo <jaimedargallo@gmail.com>
2022-07-04 11:01:20 +0200

    Add files via upload

---
+++ python.py
@@ -0,0 +1,113 @@
+
+
+# <cadena>[inicio:]
+# <cadena>[:fin]
+# <cadena>[:]
+
+sentence = "Hello World!"
+sentence[3:7]
+sentence[2:4:7]
+
+# Métodos importantes
+
+find
+index
+lower
+upper
+
+isalnum
+isalpha
+isdecimal
+isdigit
+islower
+isupper
+
+# Los métodos que comienzan por is devolerán un valor Booleano
+
+
+#Operadores aritméticos
+
+# Suma
+
+5 + 6
+
+3.4 + 5.6
+
+"Hello" + " " + "World!" #Concadenar
+
+# Resta
+
+10 - 4
+0 - 6
+3.4 - 5.6
+2 - 8.9
+"Hello" + " " + "World!" #Concadenar
+
+
+# Multiplicación
+
+5 * 6
+4 * 0
+-5 * -8
+-5 * 8
+4.5 * 7.9 
+
+
+# División (siempre devuelve un valor de coma flotante)
+
+15 / 5
+15 / 8
+12.5 / 4
+4 / 4.45
+4.2 / 4.1
+
+15 // 5
+3 // 5
+157 // 56
+-4 // -6
+-15 // -10
+
+56 / 3.4
+5.6 // 3.4
+#Util en el algoritmo de busqueda binaria
+
+
+# Exponente
+
+5 ** 3
+3 ** 8
+2 ** 3
+4.5 ** 4.7
+16 ** (1/2)
+5 ** 0
+
+# Modulo
+5 % 2
+4 % 3
+16 % 6 
+18 % 4
+
+
+# Orden de resolución
+# ----> PEMDAS <----
+# Parentesis
+# Exponentes
+# Multiplicación
+# División
+# Suma ( adición)
+# Resta (sustracción)
+
+# Condicionales
+
+temp = 15
+
+if temp <= 0:
+    print ("Muy Frio")
+elif temp < 25:
+    print ("Frio")
+else:
+    print ("Calor")
+
+    
+
+ 
